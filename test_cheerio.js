49f6ab5b7a337361eceb4e4ef83e85fdc7a3b368
OwlFlyFine <32369468+OwlFlyFine@users.noreply.github.com>
2017-11-18 16:20:16 +0700

    edit file ignore and code cheerio

--- test_cheerio.js
+++ test_cheerio.js
@@ -1,40 +1,37 @@
-const async = require("async");
-const request = require("request");
-const fs = require("fs");
+const async = require("async")
+const request = require("request")
+const fs = require("fs")
 const cheerio = require('cheerio')
 
-const urls = [
-  {
-    name:'murad',
-    url:"http://rov.wikia.com/wiki/Murad"
+const urls = [{
+    name: 'murad',
+    url: "http://rov.wikia.com/wiki/Murad"
   },
   {
-    name:'zuka',
-    url:"http://rov.wikia.com/wiki/Zuka"
+    name: 'zuka',
+    url: "http://rov.wikia.com/wiki/Zuka"
   }
 
 ]
 
-let i = 0;
-
 const queue = async.queue((task, callback) => {
   request(task.url, (error, response, body) => {
-      const $ = cheerio.load(body)
-      const text = $('#mw-content-text p').text()
+    const $ = cheerio.load(body)
+    const text = $('#mw-content-text p').text()
 
-    fs.writeFile( task.name + ".txt", text, err => {
+    fs.writeFile(task.name + ".txt", text, (err) => {
       if (err) {
-        console.log(err);
-        callback();
+        console.log(err)
+        callback()
       }
-      console.log("save file complete");
-      callback();
-    });
-  });
-}, 1);
+      console.log("save file complete")
+      callback()
+    })
+  })
+}, 1)
 
 queue.drain = () => {
-  console.log("Complete");
-};
+  console.log("Complete")
+}
 
-queue.push(urls);
+queue.push(urls)
